327efeceb25be4abd56a0ebc80cbe1431a9047dd
Kaidong Hu <dongforregister@gmail.com>
2018-09-29 14:18:38 -0400

    update init

--- my_pthread.c
+++ my_pthread.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <signal.h>
-#include <sys/ucontext.h>
 
 #include "my_pthread.h"
 
@@ -8,7 +7,18 @@
 my_pthread_ptr QThreadH, QThreadM, QThreadL;
 ucontext_t SCHED_CTX;
 
-void sched_init() {
+void __sched_init() {
   getcontext(&SCHED_CTX);
+  sigaddset(&(SCHED_CTX.uc_sigmask), SIGALARM);
+  makecontext(&SCHED_CTX, __sched_run_next);
 
+}
+
+void __sched_run_next() {
+
+}
+
+int main() {
+
+  return 0;
 }
