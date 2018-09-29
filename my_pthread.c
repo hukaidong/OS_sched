276c2fbd2e0122083a3e6aa1a4944c4af367d49b
Kaidong Hu <dongforregister@gmail.com>
2018-09-29 14:25:46 -0400

    .

--- my_pthread.c
+++ my_pthread.c
@@ -3,15 +3,16 @@
 
 #include "my_pthread.h"
 
+void __sched_init();
+void __sched_run_next();
 
 my_pthread_ptr QThreadH, QThreadM, QThreadL;
 ucontext_t SCHED_CTX;
 
 void __sched_init() {
   getcontext(&SCHED_CTX);
-  sigaddset(&(SCHED_CTX.uc_sigmask), SIGALARM);
-  makecontext(&SCHED_CTX, __sched_run_next);
-
+  sigaddset(&(SCHED_CTX.uc_sigmask), SIGALRM);
+  makecontext(&SCHED_CTX, __sched_run_next, 0);
 }
 
 void __sched_run_next() {
