2aeb3d1d3dc507cdb5612e5201d7fa603be10e75
Kaidong Hu <dongforregister@gmail.com>
2018-09-30 22:34:51 -0400

    .

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -1,13 +1,18 @@
 #ifndef MY_PTHREAD_INCLUDE
 #define MY_PTHREAD_INCLUDE
 
-#define USE_MY_PTHREAD 1
+//#define USE_MY_PTHREAD 1
+
+#ifdef USE_MY_PTHREAD
 
 #include "src/my_pthread.h"
 
 #include "sched.c"
 #include "mutex.c"
 #include "my_pthread.c"
+
+#endif /* ifdef USE_MY_PTHREAD */
+
 
 #endif /* ifndef MY_PTHREAD_INCLUDE */
 
