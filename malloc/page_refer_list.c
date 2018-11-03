a2afc7b3e452d96a0ef440ed21c4e28c38da5175
Kaidong Hu <dongforregister@gmail.com>
2018-11-03 13:44:22 -0400

    make it more buggy

--- malloc/page_refer_list.c
+++ malloc/page_refer_list.c
@@ -1,38 +1,36 @@
+#include "include/type.h"
 
-#include "include/page_refer_list.h"
-
-void insert(struct Node **head_ref, int k, int value){
-    Struct Node * new_node = (struct Node*) malloc (sizeof(struct Node));
-    new_node->k = k;
-    new_node->v = value;
-    new_node->next = *head_ref;
-    *head_ref = new_node;
-    
+void insert(Node **head_ref, int k, int value){
+  Node *new_node = (Node*)malloc (sizeof(struct Node));
+  new_node->k = k;
+  new_node->v = value;
+  new_node->next = *head_ref;
+  *head_ref = new_node;
 }
 
-int pop(struct Node *head, int k){
-    Struct Node * new_node = (struct Node*) malloc (sizeof(struct Node));
-    int val;
-    if(head->k == k){
-        val = head->v;
-        head->k = head->next->k;
-        head->v = head->next->v;
-        new_node = head->next;
-        head->next = head->next->next;
-        free(new_node)
-        return val;
+int pop(Node *head, int k){
+  Node *new_node = (Node*)malloc (sizeof(struct Node));
+  int val;
+  if(head->k == k){
+    val = head->v;
+    head->k = head->next->k;
+    head->v = head->next->v;
+    new_node = head->next;
+    head->next = head->next->next;
+    free(new_node);
+    return val;
+  }
+  else{
+    Node *prev = head;
+    while(prev->next!=NULL && prev->next->k != k)
+      prev = prev->next;
+    if(prev->next == NULL){
+      return -1; //not k found in the linkedlist
     }
-    else{
-        struct Node *prev = head;
-        while(prev->next!=null && prev->next.k!= k)
-            prev = prev->next;
-        if(prev->next == null){
-            return -1; //not k found in the linkedlist
-        }
-        
-        val = prev->next->v;
-        prev->next = prev->next->next;
-        free(new_node);
-        return val;
-    }
+
+    val = prev->next->v;
+    prev->next = prev->next->next;
+    free(new_node);
+    return val;
+  }
 }
