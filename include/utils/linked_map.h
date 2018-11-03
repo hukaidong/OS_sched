b95048584ec6cc616f4fd64a8fc153f4e77ac03f
Kaidong Hu <dongforregister@gmail.com>
2018-11-03 19:59:51 -0400

    mig

--- include/utils/linked_map.h
+++ include/utils/linked_map.h
@@ -1,21 +1,21 @@
 #ifndef MY_LINKED_MAP_H
 #define MY_LINKED_MAP_H value
-#include "include/type.h"
-#include "include/my_malloc.h"
+#include "../malloc/type.h"
+#include "../my_malloc.h"
 
-void insert(node_p &head_ref, int key, int val){
+void insert(node_p *head_ref, int key, int val) {
   node_p new_node = (node_p)_lib_malloc(sizeof(node));
   new_node->key = key;
   new_node->val = val;
-  new_node->next = head_ref;
-  head_ref = new_node;
+  new_node->next = *head_ref;
+  *head_ref = new_node;
 }
 
-int pop(node_p &head, int key, int &val){
-  node_p *last = &head, current = head;
+int pop(node_p *head, int key, int *val){
+  node_p *last = head, current = *head;
   while (current != NULL) {
     if (current->key == key) {
-      val = current->val;
+      *val = current->val;
       *last = current->next;
       _lib_free(current);
       return 1;
